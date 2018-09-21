c7330b99a2a2cebb7a1d7dc8b75972aa3c6c21a5
Evan Typanski <evantypanski@gmail.com>
2018-09-21 12:43:59 -0400

    Add user data pull from github

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	flag "github.com/ogier/pflag"
+)
+
+// Flags
+var (
+	user string
+)
 
 func main() {
-  fmt.Println("Hello, World")
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+    printUsage()
+    os.Exit(1)
+	}
+
+  users := strings.Split(user, ",")
+	fmt.Printf("Search user(s): %s\n", users)
+
+  for _, u := range users {
+    result := getUsers(u)
+    fmt.Println(`Username: `, result.Login)
+    fmt.Println(`Name: `, result.Name)
+    fmt.Println(`Email: `, result.Email)
+    fmt.Println(`Bio: `, result.Bio)
+    fmt.Println("")
+  }
+}
+
+func init() {
+	flag.StringVarP(&user, "user", "u", "", "Search Users")
+}
+
+func printUsage() {
+  fmt.Printf("Usage: %s [options]\n", os.Args[0])
+  fmt.Println("Options:")
+  flag.PrintDefaults()
 }
